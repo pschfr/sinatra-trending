afb185e70de5f3dba0903365ada60b5ad81d1e9d
Paul Schaefer <yeppaul2@gmail.com>
2019-02-02 21:09:26 -0500

    Updated Bootstrap dependency

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,6 @@
 //= require jquery-rails-4.3.3/vendor/assets/javascripts/jquery3
 //= require popper_js-1.14.5/assets/javascripts/popper
-//= require bootstrap-4.1.3/assets/javascripts/bootstrap
+//= require bootstrap-4.2.1/assets/javascripts/bootstrap
 //= require rails-assets-bootstrap-table-1.12.1/app/assets/javascripts/bootstrap-table/bootstrap-table
 //= require rails-assets-bootstrap-table-1.12.1/app/assets/javascripts/bootstrap-table/extensions/key-events/bootstrap-table-key-events
 //= require javascripts/languages
