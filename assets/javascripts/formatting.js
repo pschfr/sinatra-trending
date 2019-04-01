47976c3148fcb6ca2781a7a698df64e25cce76e5
Paul Schaefer <yeppaul2@gmail.com>
2019-04-01 19:12:27 -0400

    Added new feature: avatars for each user from GitHub

--- assets/javascripts/formatting.js
+++ assets/javascripts/formatting.js
@@ -6,3 +6,15 @@ function comma_formatter(value) {
 	var nf = new Intl.NumberFormat();
 	return nf.format(value);
 }
+
+// This takes in "pschfr/sinatra-trending", and outputs a link to that user.
+function name_to_avatar_img(project_name) {
+	// Drops everything before the slash
+	var username = project_name.split('/')[0].trim();
+	// Create a link to the GitHub user account with an image URL for the avatar for that user
+	return `
+		<a href="https://github.com/${username}" target="_blank" title="Link to ${username} on GitHub">
+			<img src="https://github.com/${username}.png?size=150" alt="Image of ${username}" class="avatar" />
+		</a>
+	`;
+}
